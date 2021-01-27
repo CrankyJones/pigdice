70d83b6b1a7dc70ca3ac5db489cf98dee4e0f464
Geof Rosenmund <geof.rosenmunds.email@gmail.com>
2021-01-27 11:17:35 -0800

    Update testing, start working on game logic
    Co-authored-by: Nick Lindau <nicholaithegreat@gmail.com>

--- README.md
+++ README.md
@@ -1,4 +1,35 @@
 Test 1
 Describe: Game()
+
 Test: Constructor that will create a new game object
 Expect Game().toEqual Game(Round)
+
+Describe: Player()
+
+Test: constructor will create a new player object with name, player number, & player score
+Expect: Player().toEqual ({Player1 = name: "Player1", playerNumber: "1", score:0})
+
+Describe: dieRoll()
+
+Test: Will output a number between 1-6
+Expect: dieRoll().toEqual dieRoll(4)
+
+Test: Will add die rolls with  the value 2-6 to user's score
+Expect: dieRoll(4).toEqual turnCount(+= 4)
+
+Test: Will recognize user victory when user score value equals 100 or more
+Expect: (player1.score => 100).toEqual($("#victory").show)
+
+Test: Will set user turnCount to 0 if user rolls a value of 1
+Expect: dieRoll(1).toEqual turnCount = 0
+
+Test: Will end turn if user rolls a value of 1 
+Expect: dieRoll(1).toEqual endTurn()
+
+Describe: endTurn()
+
+Test: Will apply user's turnCount() value to that player's player-object
+Expect: endTurn(turnCount += 4).toEqual player1.score = 4
+
+Test: Will show users score on an html scoreboard
+Expect: Endturn(player1.score = 4).toEqual ($("#player1score").show)
--- js/scripts.js
+++ js/scripts.js
@@ -1,16 +1,65 @@
 // Pig dice
 // Game object {round,}
-// Player object {Name, Score}
+// Player object {Name, Score, designation}
 // Die Array
 
 //Functions (Die roll)
-  // if rolled = 2-6
-    // turnCount += dieRoll
-    // show point total of roll
-  // else 
-    // turnCount = 0
-    //end turn
+//let rolled =
+// Math.floor(Math.random() * 7)
+// const randomElement = array[Math.floor(Math.random() * array.length)];
+// if rolled = 2-6
+// turnCount += dieRoll
+// show point total of roll
+// else 
+// turnCount = 0
+//end turn
 
 // Function (endTurn)
-  // player1.score += turnCount
-  // update scoreboard
+// player1.score += turnCount
+// if player1.score >= 100 end game
+// turnCount = 0
+// update scoreboard
+
+// function Game() {    // New Game Button
+//   this.round = 0;
+//   this.playerOne = 0;
+//   this.playerTwo = 0;
+// }
+
+function Player(name, score)  {
+  this.playerName = playerName;
+  this.score = score;
+}
+
+let turnCount = 0;
+Game.prototype.dieRoll = function () { // Roll Button
+  let rolled = Math.floor(Math.random() * 6) + 1;
+  //document.getElementById("demo").innerHTML = "rolled"; 
+  if (rolled >= 2) {
+    turnCount += rolled;
+  } else {
+    turnCount = 0;
+    endTurn();
+  }
+}
+
+Game.prototype.endTurn = function () { // Hold Button
+  if (parseInt(this.round) % 2 = 0) {
+    this.score += turnCount;
+    if (this.score >= 100) {
+      // some win game screen
+      // end game
+    }
+    turnCount = 0;
+    //update scoreboard with Game.player scores
+  } else {
+    this.playerTwo += turnCount;
+    if (this.playerTwo >= 100) {
+      // some win game screen
+      // end game
+    }
+    turnCount = 0;
+    //update scoreboard with Game.player scores
+  }
+  this.round++;
+}
