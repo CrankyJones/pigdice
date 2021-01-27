14c35a58979a835afce65b1cb96c394ae3a8c652
Geof Rosenmund <geof.rosenmunds.email@gmail.com>
2021-01-27 10:01:16 -0800

    Create basic idea flow and create tests in README
    Co-authored-by: Nick Lindau <nicholaithegreat@gmail.com>

---
+++ README.md
@@ -0,0 +1,4 @@
+Test 1
+Describe: Game()
+Test: Constructor that will create a new game object
+Expect Game().toEqual Game(Round)
---
+++ js/scripts.js
@@ -0,0 +1,16 @@
+// Pig dice
+// Game object {round,}
+// Player object {Name, Score}
+// Die Array
+
+//Functions (Die roll)
+  // if rolled = 2-6
+    // turnCount += dieRoll
+    // show point total of roll
+  // else 
+    // turnCount = 0
+    //end turn
+
+// Function (endTurn)
+  // player1.score += turnCount
+  // update scoreboard
